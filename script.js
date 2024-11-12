2ac39c79719d4c71316eb8cb08531941a80b3d20
Erica Viana <96803212+ericaviana12@users.noreply.github.com>
2024-11-12 19:41:31 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+/**
+ * Função para calcular o volume do aquário
+ * @author Erica Viana
+ */
+function calcularVolume(event) {
+    event.preventDefault()
+
+    // Captura os valores de entrada
+    let comprimento = Number(document.getElementById('comprimento').value)
+    let largura = Number(document.getElementById('largura').value)
+    let altura = Number(document.getElementById('altura').value)
+
+    // Cálculo do volume
+    let volume = (comprimento * largura * altura) / 1000
+
+    // Exibição do resultado
+    document.getElementById('resultado').value = `Volume do aquário: ${volume.toFixed(2)} litros`
+}
